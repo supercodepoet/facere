f0b2360ae2c1361433b8211952c1089f09364a8a
Travis Chase <travis@travischase.me>
2026-03-22 20:07:45 -0500

    Editing fixes

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -1,13 +1,21 @@
 class ChecklistItemsController < ApplicationController
   before_action :set_todo_list
   before_action :set_todo_item
-  before_action :set_checklist_item, only: %i[destroy toggle]
+  before_action :set_checklist_item, only: %i[update destroy toggle]
 
   def create
     @checklist_item = @todo_item.checklist_items.build(checklist_item_params)
     @checklist_item.position = @todo_item.checklist_items.count
 
     if @checklist_item.save
+      redirect_to todo_list_todo_item_path(@todo_list, @todo_item)
+    else
+      redirect_to todo_list_todo_item_path(@todo_list, @todo_item), alert: @checklist_item.errors.full_messages.first
+    end
+  end
+
+  def update
+    if @checklist_item.update(checklist_item_params)
       redirect_to todo_list_todo_item_path(@todo_list, @todo_item)
     else
       redirect_to todo_list_todo_item_path(@todo_list, @todo_item), alert: @checklist_item.errors.full_messages.first
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "lexxy" # @0.4.0
+pin "@rails/activestorage", to: "activestorage.esm.js"
+pin "lexxy", to: "lexxy.min.js"
